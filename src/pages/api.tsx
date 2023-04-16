378e43e835c7c0aa63b29e08b604f458232ca549
Federico <federicobono99@gmail.com>
2023-04-16 16:40:36 +0200

    Fix api

--- src/pages/api.tsx
+++ src/pages/api.tsx
@@ -1,25 +1,36 @@
-import React, {Component} from "react";
-import 'rapidoc';
+import React, {Component, useEffect} from "react";
 import Layout from '@theme/Layout';
+import BrowserOnly from "@docusaurus/BrowserOnly";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
-export default class ApiReferencePage extends Component {
-    render() {
-        return (
-            <Layout title="API Reference">
-                {/*@ts-ignore*/}
-                <rapi-doc
-                    spec-url="https://raw.githubusercontent.com/quattroapp/docs/master/openapi.yaml"
-                    render-style="focused"
-                    update-route={false}
-                    style={{height: "calc(100vh - 60px)", width: "100%"}}
-                    show-header={false}
-                    primary-color={'#081B34'}
-                    nav-bg-color={'#F5F5F5'}
-                    theme={'light'}
-                    allow-server-selection={false}
-                    allow-try={false}
-                />
-            </Layout>
-        )
-    }
+export default function ApiReferencePage() {
+    useEffect(() => {
+        if (ExecutionEnvironment.canUseDOM) {
+            require('rapidoc')
+        }
+    }, [])
+
+    return (
+        <Layout title="API Reference">
+            <BrowserOnly>
+                {() => (
+                    <>
+                        {/*@ts-ignore*/}
+                        <rapi-doc
+                            spec-url="https://raw.githubusercontent.com/quattroapp/docs/master/openapi.yaml"
+                            render-style="focused"
+                            update-route={false}
+                            style={{height: "calc(100vh - 60px)", width: "100%"}}
+                            show-header={false}
+                            primary-color={'#081B34'}
+                            nav-bg-color={'#F5F5F5'}
+                            theme={'light'}
+                            allow-server-selection={false}
+                            allow-try={false}
+                        />
+                    </>
+                )}
+            </BrowserOnly>
+        </Layout>
+    )
 }
